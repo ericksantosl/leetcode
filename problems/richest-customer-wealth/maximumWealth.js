54ddc11d0f3bfa60e78cb863b229b59c998c046c
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-04-02 08:12:43 -0300

    Create maximumWealth.js

---
+++ problems/richest-customer-wealth/maximumWealth.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let max = 0
+    for (let i = 0 ; i < accounts.length ; i++) {
+        let sum = 0
+        for (let j = 0 ; j < accounts[i].length; j++) {
+            sum += accounts[i][j]
+        }
+        if (sum > max) {
+            max = sum
+        }
+    }
+    return max
+};
+
+console.log(maximumWealth([[1,5],[7,3],[3,5]]))
