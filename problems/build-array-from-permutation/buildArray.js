15697e882ea135c6a183c83149d5bb340f4436da
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-04-06 10:21:33 -0300

    Create buildArray.js

---
+++ problems/build-array-from-permutation/buildArray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    let ans = []
+    for (let i = 0 ; i < nums.length ; i++) {
+        ans.push(nums[nums[i]])
+    }
+    return ans
+};
+
+
+console.log(buildArray([0,2,1,5,3,4]))
